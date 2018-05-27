b6033dd206b616626438846f2d9eb71729d82ac0
YuvalNag <33052299+YuvalNag@users.noreply.github.com>
2018-05-27 17:53:36 +0300

    gg

--- targil 5/targil 5/SymbolTable.d
+++ targil 5/targil 5/SymbolTable.d
@@ -27,15 +27,45 @@ class SymbolTable
 		methodST.clear();
 	}
 
-	void define(string name,string type,Kind kind)
+
+
+	int VarCount (Kind kind)
 	{
-       switch(kind)
-	   {
-		 case Kind.STATIC:
-
-	   }
-
+		switch(kind)
+		{
+			case Kind.STATIC:
+				return staticIndex;
+			case Kind.FIELD:
+				return fieldIndex;
+			case Kind.VAR:
+				return varIndex;
+			case Kind.ARG:
+				return argIndex;
+				break;
+			default:
+				return 0;
+ 		}
 	}
+
+	Kind KindOf(string name)
+	{
+		
+		if(name in methodST)
+			return methodST[name].kind;
+		else if(name in classST)
+			return classST[name].kind;
+		else
+			return Kind.NONE;
+	}
+
+	int KindOf(string name)
+	{
+		if(name in methodST)
+			return methodST[name].index;
+		else
+			return classST[name].index;
+	}
+
 
 }
 
