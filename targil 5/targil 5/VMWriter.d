e5169ecb98c38a95c47935dd4839a44b8f6f0dc0
nissan <nissy34@gmail.com>
2018-05-27 18:54:57 +0300

    dsd

--- targil 5/targil 5/VMWriter.d
+++ targil 5/targil 5/VMWriter.d
@@ -1,29 +1,58 @@
 module VMWriter;
 
-enum SEGMENT {STATIC,THIS,CONST,LOCAL,ARG,TEMP,POINTER,THAT};
+
+import std.conv;
+import std.stdio;
+import std.algorithm;
+import std.array;
+import std.file;
+import std.string;
+import std.path;
+import std.uni;
+import std.ascii;
+
+
+immutable enum SEGMENT {STATIC,THIS,CONST,LOCAL,ARG,TEMP,POINTER,THAT};
+enum Commands {ADD,SUB ,NEG ,EQ, GT, LT ,AND ,OR ,NOT};
+
+ 
 
 class VMWriter
 {
 
 	File vmFile;
-	string[SEGMENT] segments=[
-		STATIC:"static",
-		THIS:"this",
-		CONST:"constant",
-		LOCAL:"local",
-		ARG: "argument",
-		TEMP:"temp",
-		POINTER:"pointer",
-		THAT:"that"
-	];
+
+	    string[SEGMENT] segments;
+		
 
 	this(string path)
 	{
 		vmFile =File(chainPath(chomp(dirName(path)),baseName(path,".jack")~".vm"),"w");
+        segments[SEGMENT.STATIC ]="static";
+		segments[SEGMENT.THIS   ]="this";
+		segments[SEGMENT.CONST  ]="constant";
+		segments[SEGMENT.LOCAL  ]="local";
+		segments[SEGMENT.ARG    ]="argument";
+		segments[SEGMENT.TEMP   ]="temp";
+		segments[SEGMENT.POINTER]="pointer";
+		segments[SEGMENT.THAT   ]="that";
 	}
 
 	void writePush()
 	{
 
 	}
+
+	void writeArithmetic(Commands command)
+	{
+       vmFile.writeln(to!string(command).toLower());
+	}
+
+aw
+
+	void close(){
+		vmFile.close();
+	}
+
+
 }
