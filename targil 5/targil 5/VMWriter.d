3f157e3e90b65a5cb080de3e9c8bf53760b47913
YuvalNag <33052299+YuvalNag@users.noreply.github.com>
2018-05-27 18:51:22 +0300

    xcv

--- targil 5/targil 5/VMWriter.d
+++ targil 5/targil 5/VMWriter.d
@@ -1,5 +1,14 @@
 module VMWriter;
 
+import std.conv;
+import std.stdio;
+import std.algorithm;
+import std.array;
+import std.file;
+import std.string;
+import std.path;
+import std.uni;
+
 enum SEGMENT {STATIC,THIS,CONST,LOCAL,ARG,TEMP,POINTER,THAT};
 
 class VMWriter
@@ -7,14 +16,14 @@ class VMWriter
 
 	File vmFile;
 	string[SEGMENT] segments=[
-		STATIC:"static",
-		THIS:"this",
-		CONST:"constant",
-		LOCAL:"local",
-		ARG: "argument",
-		TEMP:"temp",
-		POINTER:"pointer",
-		THAT:"that"
+		SEGMENT.STATIC:"static",
+		SEGMENT.THIS:"this",
+		SEGMENT.CONST:"constant",
+		SEGMENT.LOCAL:"local",
+		SEGMENT.ARG: "argument",
+		SEGMENT.TEMP:"temp",
+		SEGMENT.POINTER:"pointer",
+		SEGMENT.THAT:"that"
 	];
 
 	this(string path)
@@ -22,8 +31,47 @@ class VMWriter
 		vmFile =File(chainPath(chomp(dirName(path)),baseName(path,".jack")~".vm"),"w");
 	}
 
-	void writePush()
+	void writePush(SEGMENT segment,int index)
 	{
-
+		vmFile.writeln("push ",segments[segment]," "~to!string(index));
 	}
+
+	void writePop(SEGMENT segment,int index)
+	{
+		vmFile.writeln("pop ",segments[segment]," "~to!string(index));
+	}
+
+	void writeLabel(string label)
+	{
+		vmFile.writeln("label ",label);
+	}
+
+	void writeGoto(string label)
+	{
+		vmFile.writeln("goto",label);
+	}
+
+	void writeIf(string label)
+	{
+		vmFile.writeln("if-goto",label);
+	}
+
+	void writeCall(string name,int nArgs)
+	{
+		vmFile.writeln("call",name," "~to!string(nArgs));
+	}
+
+	void writeFunction(string name,int nLocals)
+	{
+		vmFile.writeln("function",name," "~to!string(nLocals));
+	}
+
+	void writeReturn()
+	{
+		vmFile.writeln("return");
+	}
+
+
+
+
 }
